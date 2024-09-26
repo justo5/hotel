3793a71b3ae4c0594100e7737969db0d4b02ba18
Rocio Benavidez <166665317+RocioBeavidez@users.noreply.github.com>
2024-09-25 21:32:40 -0300

    Avance segunda entrega

--- hotel/src/main/java/com/mycompany/hotel/DAO/pasajeroDAO.java
+++ hotel/src/main/java/com/mycompany/hotel/DAO/pasajeroDAO.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.hotel.DAO;
 
-/**
- *
- * @author justcode
- */
-public class pasajeroDAO {
+import DAO.DAO;
+import com.mycompany.hotel.models.pasajero;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class pasajeroDAO implements DAO<pasajero>{
+
+    private static pasajeroDAO instance;
+    private List<pasajero> pasajeros = new ArrayList();
+    
+    private pasajeroDAO(){
+        
+    }
+    public static pasajeroDAO getInstance(){
+        if(pasajeroDAO.instance == null){
+            pasajeroDAO.instance = new pasajeroDAO();
+            }
+        return pasajeroDAO.instance;
+           
+        
+    }
+            
+    @Override
+    public void crear(pasajero dato) throws SQLException {
+        
+    }
+
+    @Override
+    public void actualizar(pasajero dato, int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void borrar(pasajero dato) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void borrar(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public pasajero recuperarPorId(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<pasajero> recuperarTodos() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
